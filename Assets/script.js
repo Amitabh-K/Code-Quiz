d75c324c76f102fcc831d0c6226693076bdc0d3c
Amitabh Kumar <amitabh.kumar2@gmail.com>
2019-11-17 03:37:50 -0500

    adding DOM element

---
+++ Assets/script.js
@@ -0,0 +1,39 @@
+// DOM elements 
+const timerCount = document.querySelector("#timerCount");
+
+const ifQuizSet1 = document.querySelector("#quizSet1");
+const ifQuizSet2 = document.querySelector("#quizSet2");
+const quizSetSelect = document.querySelector("#quizSetSelect");
+const ifStartQuiz = document.querySelector("#startYourQuiz");
+const ifViewHighScores = document.querySelector("#viewHighScores");
+
+const btnStartQuiz = document.querySelector("#startQuiz");
+const btnNextQuestion = document.querySelector("#nextQuestion");
+const btnReStartQuiz = document.querySelector("#reStartQuiz");
+const btnSubmit = document.querySelector("#saveScore");
+const btnClear = document.querySelector("#clear");
+
+const cardHeader = document.querySelector(".card-header");
+const initialPage = document.querySelector("#initial");
+const quizTestPage = document.querySelector("#quizTest");
+const scoreDisplayPage = document.querySelector("#scoreDisplay");
+
+const userName = document.querySelector("#nameUser");
+const userScoreDisplay = document.querySelector("#userScoreDisplay");
+const highscoreDisp = document.querySelector("#highscoreDisp");
+
+// const quesExtraDisp = document.querySelector("#quesExtraDisp");
+const questionDisp = document.querySelector("#questionDisp");
+const select1 = document.querySelector("#selection1");
+const select2 = document.querySelector("#selection2");
+const select3 = document.querySelector("#selection3");
+const select4 = document.querySelector("#selection4");
+
+const selectBox1 = document.querySelector("#boxCheckedBox1");
+const selectBox2 = document.querySelector("#boxCheckedBox2");
+const selectBox3 = document.querySelector("#boxCheckedBox3");
+const selectBox4 = document.querySelector("#boxCheckedBox4");
+
+const submitMsg = document.querySelector("#displayMsg");
+const msgDis = document.querySelector("#msgDis");
+const userScoreDis = document.querySelector("#userScore");
