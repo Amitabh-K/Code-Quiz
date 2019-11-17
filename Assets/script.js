ce6074f7873cc5af4de1ac4866dea7ae6f41164f
Amitabh Kumar <amitabh.kumar2@gmail.com>
2019-11-17 04:13:55 -0500

    adding js for timer intitioan view high sc and score

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,4 @@
 // DOM elements 
-const timerCount = document.querySelector("#timerCount");
-
-const ifQuizSet1 = document.querySelector("#quizSet1");
-const ifQuizSet2 = document.querySelector("#quizSet2");
-const quizSetSelect = document.querySelector("#quizSetSelect");
-const ifStartQuiz = document.querySelector("#startYourQuiz");
-const ifViewHighScores = document.querySelector("#viewHighScores");
-
 const btnStartQuiz = document.querySelector("#startQuiz");
 const btnNextQuestion = document.querySelector("#nextQuestion");
 const btnReStartQuiz = document.querySelector("#reStartQuiz");
@@ -21,9 +13,15 @@ const scoreDisplayPage = document.querySelector("#scoreDisplay");
 const userName = document.querySelector("#nameUser");
 const userScoreDisplay = document.querySelector("#userScoreDisplay");
 const highscoreDisp = document.querySelector("#highscoreDisp");
+const timerCount = document.querySelector("#timerCount");
 
-// const quesExtraDisp = document.querySelector("#quesExtraDisp");
+const ifQuizSet1 = document.querySelector("#quizSet1");
+const ifQuizSet2 = document.querySelector("#quizSet2");
+const quizSetSelect = document.querySelector("#quizSetSelect");
+const ifStartQuiz = document.querySelector("#startYourQuiz");
+const ifViewHighScores = document.querySelector("#viewHighScores");
 const questionDisp = document.querySelector("#questionDisp");
+
 const select1 = document.querySelector("#selection1");
 const select2 = document.querySelector("#selection2");
 const select3 = document.querySelector("#selection3");
@@ -37,3 +35,59 @@ const selectBox4 = document.querySelector("#boxCheckedBox4");
 const submitMsg = document.querySelector("#displayMsg");
 const msgDis = document.querySelector("#msgDis");
 const userScoreDis = document.querySelector("#userScore");
+
+
+//initiation
+function initPage(e){
+	// timerDisplay(countTimer);
+	highscoreDisp.style.display = "none";
+	initialPage.style.display = "block";
+	quizTestPage.style.display = "none";
+	scoreDisplayPage.style.display = "none";	
+}
+
+//test page is for diaplaying questions
+function quizPage(e){
+	highscoreDisp.style.display = "none";	
+	initialPage.style.display = "none";
+	quizTestPage.style.display = "block";
+	scoreDisplayPage.style.display = "none";
+	timer();//start the timer
+	nextQuestion();
+	initNext();
+}
+
+//initiation
+function ScorePage(e){
+	highscoreDisp.style.display = "none";	
+	countTimer = 0;
+	initialPage.style.display = "none";
+	quizTestPage.style.display = "none";
+	scoreDisplayPage.style.display = "block";	
+	setTimeout(userName.focus(), 50);//doesnot work
+}
+
+// for view high score
+
+function highscorePage(){
+	highscoreDisp.style.display = "block";	
+	initialPage.style.display = "none";
+	quizTestPage.style.display = "none";
+	scoreDisplayPage.style.display = "none";	
+}
+
+
+function timer(){
+	setInterval(function(e){
+		countTimer--;
+		if(countTimer>=0){
+			timerDisplay(countTimer);
+		}
+	},1000);
+}
+
+
+//for timer display
+function timerDisplay(count){
+	timerCount.textContent = count;
+}
